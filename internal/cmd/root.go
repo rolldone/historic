e2bc8bcc6c9c11dbbb84bf77708be684d96e8a46
Donny Rolanda <donny.rolanda@gmail.com>
2026-09-22 05:45:21 +0800

    chore: bump binary version to 0.2.0

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const Version = "0.1.0"
+const Version = "0.2.0"
 
 // NewRootCommand creates the root command for the Historic CLI.
 func NewRootCommand() *cobra.Command {
